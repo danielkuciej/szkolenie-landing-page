c34c86d43a240910c7e985e788f1a3edf32641d4
danielkuciej <biuro.symmetry@gmail.com>
2025-07-11 19:34:50 +0200

    update content section animation

--- script.js
+++ script.js
@@ -26,39 +26,20 @@ observe.observe(heroSectionEl);
 // CONTENT SECTION ANIMATION
 ///////////////////////////////////////////////////////////
 document.addEventListener("DOMContentLoaded", function () {
-  // Zapisz czas trwania animacji z CSS (w milisekundach)
-  // W naszym przypadku to 0.4s, czyli 400ms.
-  const animationDuration = 400;
-
   const contentBoxes = document.querySelectorAll(".content-box");
 
-  contentBoxes.forEach((boxToOpen) => {
-    const tab = boxToOpen.querySelector(".content-box-tab");
+  contentBoxes.forEach((box) => {
+    const tab = box.querySelector(".content-box-tab");
 
     tab.addEventListener("click", function () {
-      // Znajdź element, który jest aktualnie otwarty
-      const currentlyOpenBox = document.querySelector(".content-box.open");
+      const isAlreadyOpen = box.classList.contains("open");
 
-      // PRZYPADEK 1: Klikamy w ten sam element, który jest już otwarty
-      // Po prostu go zamykamy.
-      if (currentlyOpenBox && currentlyOpenBox === boxToOpen) {
-        currentlyOpenBox.classList.remove("open");
-      }
-      // PRZYPADEK 2: Inny element jest otwarty
-      // Zamykamy go, czekamy na koniec animacji i otwieramy nowy.
-      else if (currentlyOpenBox) {
-        // Najpierw zamknij stary element
-        currentlyOpenBox.classList.remove("open");
+      contentBoxes.forEach((otherBox) => {
+        otherBox.classList.remove("open");
+      });
 
-        // Użyj setTimeout, aby opóźnić otwarcie nowego elementu
-        setTimeout(() => {
-          boxToOpen.classList.add("open");
-        }, animationDuration);
-      }
-      // PRZYPADEK 3: Żaden element nie jest otwarty
-      // Po prostu otwórz kliknięty element.
-      else {
-        boxToOpen.classList.add("open");
+      if (!isAlreadyOpen) {
+        box.classList.add("open");
       }
     });
   });
