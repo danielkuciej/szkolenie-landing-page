f4f1b704e8b17a890d3f21942c0083e1fc055af0
danielkuciej <biuro.symmetry@gmail.com>
2025-07-14 22:57:09 +0200

    Mobile nav

--- script.js
+++ script.js
@@ -22,6 +22,41 @@ const observe = new IntersectionObserver(
 observe.observe(heroSectionEl);
 // console.log(observe);
 
+/////////////////////////////
+//Mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    if (href === "#" || href.startsWith("#")) e.preventDefault();
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // CONTENT SECTION ANIMATION
 ///////////////////////////////////////////////////////////
